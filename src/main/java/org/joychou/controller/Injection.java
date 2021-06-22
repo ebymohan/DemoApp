9ce45819f31ef0a72c60c8c75134720516fb2b5c
ebymohan <appsecdiv@gmail.com>
2021-06-22 06:23:34 +0530

    Major - Deleted unwanted Endpoints for DemoApp Presentation

--- src/main/java/org/joychou/controller/Injection.java
+++ src/main/java/org/joychou/controller/Injection.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 @Controller
